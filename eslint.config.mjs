d1e7bd3cf4138c249825d2290ea7abbad7a78ad3
dDeepLb <ddeeplb@gmail.com>
2024-05-19 06:06:23 +0300

    alias, eslint, and some other things

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*', '.types/**/*'],
+    plugins: { 'style': stylistic },
+    rules: {
+      'style/indent': ['warn', 2],
+      'style/quotes': ['warn', 'single'],
+      'style/semi': ['error', 'always'],
+      'style/linebreak-style': ['warn', 'unix'],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+);
