0d98b82ec0675299efe99a4391ebcfc7f5bc5db8
dDeepLb <ddeeplb@gmail.com>
2024-12-09 00:51:35 +0200

    update build script to do its job

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,13 @@ import path from 'path';
 import terser from 'terser-webpack-plugin';
 import TypescriptDeclarationPlugin from 'typescript-declaration-webpack-plugin';
 import { fileURLToPath } from 'url';
-import webpack from 'webpack';
 import 'webpack-dev-server';
 
-import packageJson from './package.json' assert { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const WEBPACK_DEV_SERVER_PORT = 1000;
-
-const testPath = 'http://localhost:' + WEBPACK_DEV_SERVER_PORT;
-const prodPath = 'https://ddeeplb.github.io/BC-Responsive';
+const WEBPACK_DEV_SERVER_PORT = 9999;
 
 const SRC_DIR = path.join(__dirname, 'src');
 const DIST_DIR = path.join(__dirname, 'dist');
@@ -22,13 +17,11 @@ const DIST_DIR = path.join(__dirname, 'dist');
  * @param {boolean} env
  * @returns {Promise<import('webpack').Configuration>}
  */
-export default async function (env) {
-  const mode = env.prod ? 'production' : 'development';
-  const modVersion = packageJson.version;
+export default async function () {
 
   return {
     entry: {
-      app: path.join(SRC_DIR, 'DeepLib.ts'),
+      app: path.join(SRC_DIR, 'deep_lib.ts'),
     },
     output: {
       filename: 'deeplib.js',
@@ -53,7 +46,7 @@ export default async function (env) {
       port: WEBPACK_DEV_SERVER_PORT,
     },
     devtool: 'source-map',
-    mode: mode,
+    mode: 'production',
     module: {
       rules: [
         {
@@ -78,28 +71,14 @@ export default async function (env) {
     },
     resolve: {
       extensions: ['.ts', '.js', '.css', '.html'],
-      alias: {
-        'lib_public': path.resolve(__dirname, 'node_modules/bc-deeplib/public/'),
-        'lib_types': path.resolve(__dirname, 'node_modules/bc-deeplib/.types/'),
-        'public': path.resolve(__dirname, 'public/'),
-        '_': SRC_DIR
-      }
     },
     optimization: {
-      minimize: true,
+      minimize: false,
       minimizer: [
         new terser(),
       ],
     },
     plugins: [
-      new webpack.DefinePlugin({
-        PUBLIC_URL: JSON.stringify(mode === 'development' ? testPath : prodPath),
-        MOD_VERSION: JSON.stringify(modVersion),
-      }),
-      new webpack.BannerPlugin({
-        banner: '/* eslint-disable */',
-        raw: true,
-      }),
       new TypescriptDeclarationPlugin({
         out: 'deeplib.d.ts',
       }),
