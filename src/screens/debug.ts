5b56d947d68c9ba7b7cdc34caca4ba0e50f04ee6
dDeepLb <ddeeplb@gmail.com>
2024-12-17 23:43:06 +0200

    update debug screen, add items to second page

--- src/screens/debug.ts
+++ src/screens/debug.ts
@@ -38,7 +38,6 @@ export class GuiDebug extends BaseSubscreen {
         id: 'test-deeplib-checkbox',
         label: 'Checkbox',
         description: 'This is a checkbox',
-        checked: false,
         getSettingValue() {
           return true;
         },
@@ -73,6 +72,73 @@ export class GuiDebug extends BaseSubscreen {
       <Label>{
         type: 'label',
         id: 'test-deeplib-label',
+        label: 'Label',
+        description: 'This is a label',
+      }
+    ],
+    [
+      <Button>{
+        type: 'button',
+        roundness: 50,
+        id: 'test-deeplib-big-button2',
+        size: [405, 80],
+        label: 'Big Button',
+        tooltip: 'This is a big button',
+        image: 'Icons/Prev.png',
+        onClick() {
+          deepLibLogger.info('Big Button Clicked');
+        }
+      },
+      <Button>{
+        type: 'button',
+        roundness: 5,
+        id: 'test-deeplib-small-button2',
+        size: [90, 90],
+        tooltip: 'This is a small button',
+        image: 'Icons/Next.png',
+        onClick() {
+          deepLibLogger.info('Small Button Clicked');
+        }
+      },
+      <Checkbox>{
+        type: 'checkbox',
+        id: 'test-deeplib-checkbox2',
+        label: 'Checkbox',
+        description: 'This is a checkbox',
+        getSettingValue() {
+          return true;
+        },
+        setSettingValue(val: boolean) {
+          deepLibLogger.info('Checkbox value:', val);
+        },
+      },
+      <Input>{
+        type: 'text',
+        id: 'test-deeplib-text-input2',
+        label: 'Input',
+        description: 'This is a text input',
+        getElementValue() {
+          return 'Input Value';
+        },
+        setSettingValue(val: string) {
+          deepLibLogger.info('Input value:', val);
+        },
+      },
+      <Input>{
+        type: 'number',
+        id: 'test-deeplib-number-input2',
+        label: 'Input',
+        description: 'This is a number input',
+        getElementValue() {
+          return '123';
+        },
+        setSettingValue(val: string) {
+          deepLibLogger.info('Input value:', val);
+        },
+      },
+      <Label>{
+        type: 'label',
+        id: 'test-deeplib-label2',
         label: 'Label',
         description: 'This is a label',
       }
