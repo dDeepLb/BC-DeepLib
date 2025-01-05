ef6c2b1f18a2c1c976d080a0eeb8c649da8806a1
dDeepLb <ddeeplb@gmail.com>
2025-01-05 02:56:19 +0200

    move style injecting functions from Themed

--- src/utilities/style.ts
+++ src/utilities/style.ts
@@ -1,24 +1,40 @@
-export class Style {
-  static inject(id: string, styleSource: string) {
-    const isStyleLoaded = !!document.getElementById(id);
+export const Style = {
+  injectInline(styleId: string, styleSource: string) {
+    const isStyleLoaded = document.getElementById(styleId);
 
     if (isStyleLoaded) return;
 
     const styleElement = document.createElement('style');
-    styleElement.id = id;
+    styleElement.id = styleId;
     styleElement.appendChild(document.createTextNode(styleSource));
     document.head.appendChild(styleElement);
-  }
+  },
 
-  static eject(id: string) {
+  injectEmbed(styleId: string, styleLink: string) {
+    const isStyleLoaded = document.getElementById(styleId);
+
+    if (isStyleLoaded) return;
+
+    const styleElement = document.createElement('link');
+    styleElement.id = styleId;
+    styleElement.rel = 'stylesheet';
+    styleElement.href = styleLink;
+    document.head.appendChild(styleElement);
+  },
+
+  eject(id: string) {
     const style = document.getElementById(id);
     if (!style) return;
 
     style.remove();
-  }
+  },
 
-  static reload(id: string, styleSource: string) {
-    Style.eject(id);
-    Style.inject(id, styleSource);
+  reload(styleId: string, styleSource: string) {
+    Style.eject(styleId);
+    Style.injectInline(styleId, styleSource);
+  },
+
+  async fetch(link: string) {
+    return fetch(link).then((res) => res.text());
   }
-}
+};
