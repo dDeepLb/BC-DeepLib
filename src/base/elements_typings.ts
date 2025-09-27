3c0490f6bc8658e92d64e4436cd32b9f5281791c
dDeepLb <ddeeplb@gmail.com>
2025-09-28 01:56:23 +0300

    prettify types

--- src/base/elements_typings.ts
+++ src/base/elements_typings.ts
@@ -7,40 +7,40 @@ export type BaseElementModel = {
   disabled?: boolean | (() => boolean);
 };
 
-export type Button = Omit<BaseElementModel, 'id'> & {
+export type Button = Prettify<{
   id: Parameters<typeof ElementButton.Create>[0];
   type: 'button';
   onClick?: Parameters<typeof ElementButton.Create>[1];
   options?: Parameters<typeof ElementButton.Create>[2];
   htmlOptions?: Parameters<typeof ElementButton.Create>[3];
-};
+} & Omit<BaseElementModel, 'id'>>;
 
-export type Checkbox = BaseElementModel & {
+export type Checkbox = Prettify<{
   type: 'checkbox';
   label?: string;
   description?: string;
   setElementValue?: () => boolean;
   setSettingValue?: (val: boolean) => void;
   htmlOptions?: Partial<Record<'container' | 'checkbox' | 'label', Omit<HTMLOptions<any>, 'tag'>>> | null | undefined;
-};
+} & BaseElementModel>;
 
-export type Input = BaseElementModel & {
+export type Input = Prettify<{
   type: 'text' | 'number' | 'color';
   label?: string;
   description?: string;
   setElementValue?: () => string;
   setSettingValue?: (val: string) => void;
   htmlOptions?: Partial<Record<'container' | 'input' | 'label', Omit<HTMLOptions<any>, 'tag'>>> | null | undefined;
-};
+} & BaseElementModel>;
 
-export type Label = BaseElementModel & {
+export type Label = Prettify<{
   type: 'label';
   label?: string;
   description?: string;
   htmlOptions?: Omit<HTMLOptions<any>, 'tag'>;
-};
+} & BaseElementModel>;
 
-export type Custom = BaseElementModel & {
+export type Custom = Prettify<{
   type: 'custom';
   htmlOptions: HTMLOptions<keyof HTMLElementTagNameMap>;
-};
+} & BaseElementModel>;
