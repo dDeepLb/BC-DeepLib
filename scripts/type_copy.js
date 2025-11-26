39782b4d97d1ec8dbf782a763e647bf8057534e3
dDeepLb <ddeeplb@gmail.com>
2025-11-26 22:11:35 +0200

    change scripts, run tsc before all to prevent bad stuff from happening :3

--- scripts/type_copy.js
+++ scripts/type_copy.js
@@ -1,5 +1,3 @@
-import npm_dts from 'npm-dts';
-const { Generator } = npm_dts;
 import fsExtra from 'fs-extra';
 
 import path from 'path';
@@ -10,15 +8,6 @@ const __dirname = path.dirname(__filename);
 
 const SRC_DIR = path.join(__dirname, '../src');
 const DIST_DIR = path.join(__dirname, '../dist');
-
-new Generator({
-  entry: path.join(SRC_DIR, 'deeplib.ts'),
-  output: path.join(DIST_DIR, 'deeplib.d.ts'),
-  tsc: `--project ${path.join(__dirname, '../tsconfig.json')}`
-})
-  .generate()
-  .then(() => console.log('Generated types'))
-  .catch(console.error);
 
 fsExtra.copy(path.join(SRC_DIR, 'vendored_types'), path.join(DIST_DIR, 'vendored_types'))
   .then(() => console.log('Copied 3rd party types'))
