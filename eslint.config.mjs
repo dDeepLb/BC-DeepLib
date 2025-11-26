4990f2044a96efad93622577dd2553611aca3bb2
dDeepLb <ddeeplb@gmail.com>
2025-11-26 22:00:48 +0200

    change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,33 +2,41 @@ import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   {
-    files: ['src/**/*', '.types/**/*', 'lib/**/*', 'scripts/**/*'],
-    plugins: { 'style': stylistic },
-    rules: {
-      'style/indent': ['warn', 2],
-      'style/quotes': ['warn', 'single'],
-      'style/semi': ['error', 'always'],
-      'style/linebreak-style': ['warn', 'unix'],
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          'args': 'all',
-          'argsIgnorePattern': '^_',
-          'caughtErrors': 'all',
-          'caughtErrorsIgnorePattern': '^_',
-          'destructuredArrayIgnorePattern': '^_',
-          'varsIgnorePattern': '^_',
-          'ignoreRestSiblings': true
-        }
-      ]
+    ignores: ['dist/']
+  },
+  {
+    files: ['lib/**/*.{js,ts}', 'scripts/**/*.{js,ts}'],
+    languageOptions: { globals: { ...globals.node } }
+  },
+  {
+    files: ['src/**/*.{js,ts}'],
+    languageOptions: { globals: { ...globals.browser } }
+  },
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      '@stylistic-eslint': stylistic
     },
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      '@stylistic-eslint/indent': ['warn', 2],
+      '@stylistic-eslint/quotes': ['warn', 'single'],
+      '@stylistic-eslint/semi': ['warn', 'always'],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        'args': 'all',
+        'argsIgnorePattern': '^_',
+        'caughtErrors': 'all',
+        'caughtErrorsIgnorePattern': '^_',
+        'destructuredArrayIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+      }],
+      'eqeqeq': ['warn', 'always'],
+    },
   },
 );
